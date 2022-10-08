d73c48244f583893c163cf5ef8c0e41ce3887b02
Samuel Rantung <samuelmrantung@gmail.com>
2022-10-08 18:04:23 +0800

    add custom text component with inter fontfamily

---
+++ src/components/atoms/TextInter.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, TextStyle} from 'react-native';
+import React from 'react';
+import {theme} from '../../assets/designSystem';
+
+type CustomTextProps = {
+  style?: TextStyle | TextStyle[];
+  children: any;
+};
+
+const TextInter: React.FC<CustomTextProps> = ({children, style}) => {
+  const passedStyles = Array.isArray(style)
+    ? Object.assign({}, ...style)
+    : style;
+
+  return <Text style={[styles.text, {...passedStyles}]}>{children}</Text>;
+};
+
+export default TextInter;
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: theme.fonts.inter.regular,
+  },
+});
